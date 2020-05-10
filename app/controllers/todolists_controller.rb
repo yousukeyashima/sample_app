d17d3f32f1caadcfc9fc622f6e9fd47a78576cc9
vagrant <vagrant@localhost.localdomain>
2020-05-10 12:17:59 +0000

    [Finish]3-6

--- app/controllers/todolists_controller.rb
+++ app/controllers/todolists_controller.rb
@@ -8,8 +8,17 @@ class TodolistsController < ApplicationController
 
   	list.save
 
-  	redirect_to "/top"
+  	redirect_to todolist_path(list.id)
   end
+
+  def index
+  	@lists = List.all
+  end
+
+  def show
+  	@list = List.find(params[:id])
+  end
+
   private
 
   def list_params
